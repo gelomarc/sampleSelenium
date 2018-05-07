88a22f5ddbebe821ece3d9d67fd6658afe4c88d0
Grzegorz Marczewski <gelomarc@gmail.com>
2018-05-07 16:33:59 +0200

    specify exception names and arguments in lambdas

--- src/main/java/utils/Waiter.java
+++ src/main/java/utils/Waiter.java
@@ -1,6 +1,7 @@
 package utils;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -20,14 +21,6 @@ public class Waiter {
         this.wait = new WebDriverWait(driver, TIMEOUT);
     }
 
-    public void waitForElementToBeVisible(WebElement element) {
-        wait.until(arg0 -> element.isDisplayed());
-    }
-
-    public void waitForElementToBeClickable(WebElement element) {
-        wait.until(arg0 -> element.isEnabled());
-    }
-
     public void waitForPageLoad() {
         wait.until(driver1 -> String.valueOf(((JavascriptExecutor) driver1).executeScript("return document.readyState")).equals("complete"));
     }
@@ -39,14 +32,14 @@ public class Waiter {
 
     public void enterText(WebElement element, WebDriver driver, String text) {
         WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
-        ExpectedCondition<Boolean> elementIsActive = arg0 ->
+        ExpectedCondition<Boolean> elementIsActive = elementToEnterText ->
         {
             try {
                 element.clear();
                 element.sendKeys(text);
                 assertEquals(text, element.getAttribute("value"));
                 return true;
-            } catch (Exception e) {
+            } catch (NoSuchElementException e) {
                 return false;
             }
         };
@@ -56,12 +49,12 @@ public class Waiter {
     public void selectFromDropdownByValue(WebElement element, WebDriver driver, String text) {
         WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
         Select select = new Select(element);
-        ExpectedCondition<Boolean> elementIsActive = arg0 ->
+        ExpectedCondition<Boolean> elementIsActive = elementToSelectFrom ->
         {
             try {
                 select.selectByValue(text);
                 return true;
-            } catch (Exception e) {
+            } catch (NoSuchElementException e) {
                 return false;
             }
         };
@@ -71,12 +64,12 @@ public class Waiter {
     public void selectFromDropdownByText(WebElement element, WebDriver driver, String text) {
         WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
         Select select = new Select(element);
-        ExpectedCondition<Boolean> elementIsActive = arg0 ->
+        ExpectedCondition<Boolean> elementIsActive = elementToSelectFrom ->
         {
             try {
                 select.selectByVisibleText(text);
                 return true;
-            } catch (Exception e) {
+            } catch (NoSuchElementException e) {
                 return false;
             }
         };
@@ -85,12 +78,12 @@ public class Waiter {
 
     public void click(WebElement element, WebDriver driver) {
         WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
-        ExpectedCondition<Boolean> elementIsClickable = arg0 ->
+        ExpectedCondition<Boolean> elementIsClickable = elementToClickOn ->
         {
             try {
                 element.click();
                 return true;
-            } catch (Exception e) {
+            } catch (NoSuchElementException e) {
                 return false;
             }
         };
